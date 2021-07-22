992ec80812f7fd72d64906195a78e4778869623c
SongJuyeon <80025101+song-juyeon@users.noreply.github.com>
2021-07-22 14:18:15 +0900

    Initial commit

---
+++ app/src/main/java/kr/hs/emirim/w2034/ex7_14_15/MainActivity.java
@@ -0,0 +1,15 @@
+package kr.hs.emirim.w2034.ex7_14_15;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
